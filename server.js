1754b06fa84498e5461f5db6e06c7c51b1aeccc0
mianak78 <rosebourn@gmail.com>
2019-12-09 14:22:04 -0700

    views working

--- server.js
+++ server.js
@@ -17,5 +17,53 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-//   runSearch();
+  console.log("connected as id " + connection.threadId + "\n");
+  askUser();
 });
+
+function askUser() {
+  inquirer
+      .prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            name: "option",
+            choices: ["View all departments", "View all roles", "View all employees", "Add department", "Add role", "Add employee", "Update employee role"]
+        }
+      ])
+
+  .then((res) => {
+    console.log(res);
+    if (res.option === "View all departments") {
+      connection.query("SELECT name FROM department", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+      })
+    }
+    if (res.option === "View all roles") {
+      connection.query("SELECT title FROM role", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+      })
+    }
+    if (res.option === "View all employees") {
+      connection.query("SELECT first_name, last_name FROM employee", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+      })
+    }
+  })
+    // if (res.option === "Add department") {
+    //   // insertDept();
+    //   function insertDept(name) {
+    //     connection.query("INSERT INTO department (name) VALUES (?)",
+    //     [name],
+    //     function (err, response) {
+    //       if (err) throw err;
+    //       console.log("\nDepartment successfully added.")
+    //     })
+    //   }
+    // }
+    //  insertDept();
+    
+}
